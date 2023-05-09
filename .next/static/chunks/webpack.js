441c6299d77e9df5e0bfa32a788fed056a4e10ea
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-05-09 16:49:30 +0300

    added goerli test network

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fcb93410ff335a3b"; }
+/******/ 		__webpack_require__.h = function() { return "c14fa7170f020c30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@
 const nextConfig = {
     /* config options here */
     env: {
-        networkName: "maticmum",
+        MaticNetworkName: "maticmum",
+        networkName: "goerli",
         primitivesAddress: "0xE3c438a87c3d3aBE58316fF7c3D345eFD9dE0462",
     }
 }
