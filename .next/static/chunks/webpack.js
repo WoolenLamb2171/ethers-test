9209d20230961705253f75452a6fa0a1e328ba62
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-07-26 17:18:10 +0300

    added condition for set my wallet

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dc74ed2dab2994db"; }
+/******/ 		__webpack_require__.h = function() { return "a2ff0c7213cce4ea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/primitives.js
+++ pages/primitives.js
@@ -217,7 +217,7 @@ const Primitives = () => {
         <label htmlFor="Set Wallet">New Wallet:</label>
         <input name="Set Wallet" ref={walletRef} type="text"></input>
         <button>Set new Wallet</button>
-        <button onClick={handleSetMyWalletClick}>Set my Wallet</button>
+        {currentAccount && <button onClick={handleSetMyWalletClick}>Set my Wallet</button>}
       </form>
 
     </div>
